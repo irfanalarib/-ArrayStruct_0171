6bbf4cc74779e539b7010fc7ae720299e7709751
irfanalarib <irfanalarib7@gmail.com>
2026-04-21 10:02:49 +0700

    copy struct 2

---
+++ Struct3.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+    string desa;
+    string kota;
+};
+
+struct Mahasiswa {
+    string NIM;
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
+
+int main()
+{
+    Mahasiswa mhs;
+
+    cout << "Nomer Mahasiswa= ";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa = ";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa = " << endl;
+    cout << "\t Nama Desa : ";
+    cin >> mhs.alamat.desa; 
+    cout << "\t Nama Kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "Umur Mahasiswa = ";
+    cin >> mhs.umur;
+
+    cout << endl;
+
+    cout << "\n NIM = " << mhs.nim << endl;
+    cout << "\n Nama = " << mhs.nama << endl;
+    cout << "\n Alamat = ";
+    cout << "\t Desa = " << mhs.alamat.desa << endl;
+    cout << "\t Kota = " << mhs.alamat.kota << endl;
+    cout << "\n Umur = " << mhs.umur << endl;
+}
