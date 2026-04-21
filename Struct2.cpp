5b0a0c3da1dae0d7b16e9d9242f2cdeadc4e25a7
irfanalarib <irfanalarib7@gmail.com>
2026-04-21 09:53:24 +0700

    menambahkan input alamat pada funvtion maiin

--- Struct2.cpp
+++ Struct2.cpp
@@ -21,8 +21,11 @@ int main()
     cin >> mhs.nim;
     cout << "Nama Mahasiswa = ";
     cin >> mhs.nama;
-    cout << "Alamat Mahasiswa = ";
-    cin >> mhs.alamat;
+    cout << "Alamat Mahasiswa = " << endl;
+    cout << "\t Nama Desa : ";
+    cin >> mhs.alamat.desa; 
+    cout << "\t Nama Kota : ";
+    cin >> mhs.alamat.kota;
     cout << "Umur Mahasiswa = ";
     cin >> mhs.umur;
 
