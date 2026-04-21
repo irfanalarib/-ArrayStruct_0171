cab2bbc1fa51e414bf302729316f1d0e1387976d
irfanalarib <irfanalarib7@gmail.com>
2026-04-21 09:47:02 +0700

    add main function

--- Struct2.cpp
+++ Struct2.cpp
@@ -12,3 +12,26 @@ struct Mahasiswa {
     DetailAlamat alamat;
     int umur;
 };
+
+int main()
+{
+    Mahasiswa mhs;
+
+    cout << "Nomer Mahasiswa= ";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa = ";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa = ";
+    cin >> mhs.alamat;
+    cout << "Umur Mahasiswa = ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "Menampilkan Data" <<endl;
+    cout << endl;
+
+    cout << "\n NIM = " << mhs.nim << endl;
+    cout << "\n Nama = " << mhs.nama << endl;
+    cout << "\n Alamat = " << mhs.alamat << endl;
+    cout << "\n Umur = " << mhs.umur << endl;
+}
